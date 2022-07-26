746221479d914e1aa092df3991e9c43444e6f48e
Julian Michel <mail@julianmichel.de>
2022-07-26 22:54:01 +0200

    chore: add cdk-nag

--- .projenrc.js
+++ .projenrc.js
@@ -6,7 +6,7 @@ const project = new awscdk.AwsCdkTypeScriptApp({
 
   prettier: true,
 
-  // deps: [],                /* Runtime dependencies of this module. */
+  deps: ["cdk-nag"] /* Runtime dependencies of this module. */,
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,30 @@
-import { App, Stack, StackProps } from "aws-cdk-lib";
+import { App, Aspects, Stack, StackProps } from "aws-cdk-lib";
+import { Bucket, BucketEncryption } from "aws-cdk-lib/aws-s3";
+import { AwsSolutionsChecks, NagSuppressions } from "cdk-nag";
 import { Construct } from "constructs";
 
 export class MyStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
-    // define resources here...
+    const myBucket = new Bucket(this, "MyBucket", {
+      encryption: BucketEncryption.S3_MANAGED,
+      blockPublicAccess: {
+        blockPublicAcls: true,
+        blockPublicPolicy: true,
+        ignorePublicAcls: true,
+        restrictPublicBuckets: true,
+      },
+      enforceSSL: true,
+      versioned: true,
+    });
+
+    NagSuppressions.addResourceSuppressions(myBucket, [
+      {
+        id: "AwsSolutions-S1",
+        reason: "ServerAccessLogs not necessary",
+      },
+    ]);
   }
 }
 
@@ -18,6 +37,8 @@ const devEnv = {
 const app = new App();
 
 new MyStack(app, "cdk-nag-demo-dev", { env: devEnv });
-// new MyStack(app, 'cdk-nag-demo-prod', { env: prodEnv });
+
+Aspects.of(app).add(new AwsSolutionsChecks());
+// Aspects.of(app).add(new NIST80053R5Checks());
 
 app.synth();
